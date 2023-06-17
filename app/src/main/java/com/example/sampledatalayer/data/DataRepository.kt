71771d89b74dfd40abcfcc7c2d66f4f915162c48
DESKTOP-9REHMCV\koyama ryuji <ryujikoyama95@gmail.com>
2023-06-17 22:58:51 +0900

    Update repository process

--- app/src/main/java/com/example/sampledatalayer/data/DataRepository.kt
+++ app/src/main/java/com/example/sampledatalayer/data/DataRepository.kt
@@ -1,4 +1,24 @@
 package com.example.sampledatalayer.data
 
-class DataRepository {
+class DataRepository(
+    private val database: SampleDatabase,
+    private var dao: SampleDao
+) {
+    companion object {
+        const val TAG = "DataRepository"
+    }
+
+    init {
+        dao = database.sampleDao()
+    }
+
+    suspend fun createData(title: String) {
+        dao.insert(title)
+    }
+    suspend fun getData() {
+        dao.getData()
+    }
+    suspend fun deleteData(id: Int) {
+        dao.delete(id)
+    }
 }
