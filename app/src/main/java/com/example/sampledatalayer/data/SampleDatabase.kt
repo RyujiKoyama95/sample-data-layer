8ece3a150cec085154800c60a1bb5207a6b66db0
DESKTOP-9REHMCV\koyama ryuji <ryujikoyama95@gmail.com>
2023-06-17 23:21:01 +0900

    Update database

--- app/src/main/java/com/example/sampledatalayer/data/SampleDatabase.kt
+++ app/src/main/java/com/example/sampledatalayer/data/SampleDatabase.kt
@@ -1,9 +1,27 @@
 package com.example.sampledatalayer.data
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(entities = [SampleData::class], version = 1, exportSchema = false)
 abstract class SampleDatabase: RoomDatabase() {
     abstract fun sampleDao(): SampleDao
+
+    companion object {
+        private var sampleDatabase: SampleDatabase? = null
+
+        fun getDatabase(context: Context): SampleDatabase {
+            return sampleDatabase ?: synchronized(this) {
+                val instanse = Room.databaseBuilder(
+                    context.applicationContext,
+                    SampleDatabase::class.java,
+                    "sample_database"
+                ).fallbackToDestructiveMigration().build()
+                sampleDatabase = instanse
+                return sampleDatabase as SampleDatabase
+            }
+        }
+    }
 }
