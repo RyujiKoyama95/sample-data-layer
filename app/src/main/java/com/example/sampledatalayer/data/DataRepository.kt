d6d624ecc4a3f00c985dbf9f33427d763d31e42d
DESKTOP-9REHMCV\koyama ryuji <ryujikoyama95@gmail.com>
2023-06-17 23:50:01 +0900

    Remove init process for create dao

--- app/src/main/java/com/example/sampledatalayer/data/DataRepository.kt
+++ app/src/main/java/com/example/sampledatalayer/data/DataRepository.kt
@@ -1,15 +1,10 @@
 package com.example.sampledatalayer.data
 
 class DataRepository(
-    private val database: SampleDatabase,
-    private var dao: SampleDao
+    private val dao: SampleDao
 ) {
     companion object {
         const val TAG = "DataRepository"
-    }
-
-    init {
-        dao = database.sampleDao()
     }
 
     suspend fun createData(data: SampleData) {
