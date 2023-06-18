1d06366ffd6ebeba731416b6f7f0ff449fcfdf22
DESKTOP-9REHMCV\koyama ryuji <ryujikoyama95@gmail.com>
2023-06-18 20:58:16 +0900

    Add view model

--- app/src/main/java/com/example/sampledatalayer/ui/SampleViewModel.kt
+++ app/src/main/java/com/example/sampledatalayer/ui/SampleViewModel.kt
@@ -1,4 +1,43 @@
-package com.example.sampledatalayer.ui.theme
+package com.example.sampledatalayer.ui
 
-class SampleViewModel {
+import android.content.Context
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.sampledatalayer.data.DataRepository
+import com.example.sampledatalayer.data.SampleDao
+import com.example.sampledatalayer.data.SampleData
+import com.example.sampledatalayer.data.SampleDatabase
+import kotlinx.coroutines.launch
+
+class SampleViewModel(
+    private val context: Context
+): ViewModel() {
+    companion object {
+        const val TAG = "SampleViewModel"
+    }
+
+    private var repo: DataRepository
+
+    init {
+        val dao = SampleDatabase.getDatabase(context).sampleDao()
+        repo = DataRepository(dao)
+    }
+
+    fun create(data: SampleData) {
+        viewModelScope.launch {
+            repo.createData(data)
+        }
+    }
+    fun get(): List<SampleData> {
+        var list = listOf<SampleData>()
+        viewModelScope.launch {
+            list = repo.getData()
+        }
+        return list
+    }
+    fun delete() {
+        viewModelScope.launch {
+            repo.deleteAllData()
+        }
+    }
 }
